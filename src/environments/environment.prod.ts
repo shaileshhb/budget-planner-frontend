57457d41a61851c4b127f57ad309762cd9966d67
shaileshhb <shaileshb.0720@gmail.com>
2022-11-30 15:01:45 +0530

    project build complete

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   SECRET_KEY: "d08h5kOZiYrsz7AzxAM6",
   // BASEURL: 'https://budget-planner.up.railway.app',
-  BASEURL: 'https://budget-planner-2n99.onrender.com',
+  BASEURL: 'https://budget-planner-2n99.onrender.com/api/v1/budget-planner',
 };
